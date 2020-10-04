916e7a7903926e893d7c6dd88af05f36689f4b4c
Veri97 <veri.kazazi1997@hotmail.com>
2020-10-04 15:41:26 +0200

    commit Adding photos to the cloud

---
+++ DatingApp.API/Dtos/PhotoForCreationDto.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class PhotoForCreationDto
+    {
+        public string Url { get; set; }
+        public IFormFile File { get; set; }      //represents the file/photo sent over http request
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public string PublicId { get; set; }
+
+        public PhotoForCreationDto()
+        {
+            DateAdded = DateTime.Now;
+        }
+    }
+
+}
